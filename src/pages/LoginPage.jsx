aa7dfd9c31b0df43e6e74d52e61e88bbb932772b
mikachou <chou1005.tw@gmail.com>
2024-05-01 17:53:29 +0800

    adjust login page layout

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,26 @@
 import "../styles/loginPage.scss";
-// import AuthInput from "../components/AuthInput";
+import AuthInput from "../components/AuthInput";
 
 export default function LogInPage() {
-  return <div id="loginPage"></div>;
+  return (
+    <div id="loginPage">
+      <div className="leftSide">
+        <img src="/src/assets/intro3.jpg" alt="" />
+      </div>
+      <div className="rightSide">
+        <div className="logoWrapper">
+          <img src="/src/assets/logo.png" alt="logo" />
+          <p>好點子手作</p>
+        </div>
+        <div className="loginWrapper">
+          <h2>請登入</h2>
+          <form action="">
+            <AuthInput />
+            <AuthInput />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }

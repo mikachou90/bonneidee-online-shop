1dc1531ff48abab3b90f92d3ba6387a46a3159e0
mikachou <chou1005.tw@gmail.com>
2024-05-01 17:19:15 +0800

    create login page and authInput component

--- src/components/AuthInput.jsx
+++ src/components/AuthInput.jsx
@@ -1,5 +1,10 @@
 import "../styles/authInput.scss";
 
 export default function AuthInput() {
-  return <div></div>;
+  return (
+    <div className="inputWrapper">
+      <label htmlFor="">LABEL</label>
+      <input type="text" />
+    </div>
+  );
 }
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,6 @@
 import "../styles/loginPage.scss";
+// import AuthInput from "../components/AuthInput";
 
 export default function LogInPage() {
-  return <div className="login"></div>;
+  return <div id="loginPage"></div>;
 }

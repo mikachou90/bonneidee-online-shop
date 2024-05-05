8aef6a1756fc49cda6ed8c2f323fc35f36ad3bf7
mikachou <chou1005.tw@gmail.com>
2024-05-05 09:51:17 +0800

    create router

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,36 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./pages/Root";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MainPage from "./pages/MainPage";
-// import AllProductPage from "./pages/AllProductPage";
-// import ProductDetailPage from "./pages/ProductDetailPage";
+import AllProductPage from "./pages/AllProductPage";
+import ProductDetailPage from "./pages/ProductDetailPage";
 // import LoginPage from "./pages/LoginPage";
-// import OrderProgress from "./pages/OrderProgress";
+import OrderProgress from "./pages/OrderProgress";
 import "./App.scss";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    component: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <MainPage />,
+      },
+      { path: "/products", element: <AllProductPage /> },
+      { path: "/products/:productId", element: <ProductDetailPage /> },
+      { path: "/order-progress", element: <OrderProgress /> },
+    ],
+  },
+]);
 
 function App() {
   return (
     <>
       <Header />
-      {/* <div id="globalWrapper"> */}
-      <MainPage />
-      {/* <AllProductPage /> */}
-      {/* <ProductDetailPage /> */}
-      {/* <OrderProgress /> */}
-      {/* </div> */}
+      <RouterProvider router={router} />
       <Footer />
-
-      {/* <LoginPage /> */}
     </>
   );
 }

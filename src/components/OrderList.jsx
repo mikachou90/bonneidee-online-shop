04c73decab86c1d37e7008fce04213ac47ef1255
mikachou <chou1005.tw@gmail.com>
2024-05-07 17:25:45 +0800

    create my-page

---
+++ src/components/OrderList.jsx
@@ -0,0 +1,8 @@
+import "../styles/OrderList.scss";
+export default function OrderList() {
+  return (
+    <div id="orderList">
+      <h4>id</h4>
+    </div>
+  );
+}
--- src/pages/user/UserPage.jsx
+++ src/pages/user/UserPage.jsx
@@ -1,12 +1,15 @@
 import "../../styles/userPage.scss";
 import { useAuth0 } from "@auth0/auth0-react";
 import useGetUser from "../../queries/useGetUser";
+import ProductCard from "../../components/ProductCard";
+import OrderList from "../../components/OrderList";
 
 const UserPage = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
   const { data: userData, isLoading: isUserLoading } = useGetUser();
 
   console.log({ userData, isUserLoading });
+  console.log({ user });
 
   if (isLoading) {
     return <div>User Loading ...</div>;
@@ -17,10 +20,26 @@ const UserPage = () => {
   }
 
   return (
-    <div>
-      <img src={user.picture} alt={user.name} />
-      <h2>{user.name}</h2>
-      <p></p>
+    <div id="userPage">
+      <div className="userGreeting">
+        <h3>Hello, {user.nickname}</h3>
+      </div>
+      <section className="userOrderList">
+        <h3>我的訂單</h3>
+        <div className="userOder">
+          <OrderList />
+        </div>
+      </section>
+      <section className="userFavItem">
+        <h3>我的最愛</h3>
+        <div className="favItem">
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+        </div>
+      </section>
     </div>
   );
 };

11528a0e0cd4324c2b03bfe53fa7b083159b949d
mikachou90 <chou1005.tw@gmail.com>
2024-05-14 10:48:49 +0800

    adjust footer layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import "../styles/footer.scss";
+import { Link } from "react-router-dom";
 import { FaInstagram } from "react-icons/fa";
 import { FaShoppingBasket } from "react-icons/fa";
 import { IoMailOutline } from "react-icons/io5";
@@ -10,6 +11,7 @@ export default function Footer() {
         <img src="/src/assets/logo.png" alt="logo"></img>
         <p>好點子手作</p>
       </div>
+
       <div className="linkWrapper">
         <p>Find us!</p>
         <div className="link">

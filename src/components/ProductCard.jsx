5bf41131b72ce50630b956118477b41560561280
mikachou <chou1005.tw@gmail.com>
2024-05-17 15:17:00 +0800

    favBtn adjusted

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -4,26 +4,28 @@ import { Link } from "react-router-dom";
 export default function ProductCard() {
   return (
     <>
-      <Link to="/products/1" className="productCardWrapper">
-        <div className="productImgWrapper">
-          <img
-            src="/src/assets/pictures/bottleHolder1.jpg"
-            alt="product photo"
-          />
-        </div>
-        <div className="productInfoWrapper">
-          <div className="productCategory">
-            <p>鑰匙圈</p>
+      <div className="productCardWrapper">
+        <Link to="/products/1">
+          <div className="productImgWrapper">
+            <img
+              src="/src/assets/pictures/bottleHolder1.jpg"
+              alt="product photo"
+            />
           </div>
-          <h4>商品名</h4>
-          <p>$100</p>
-          <div className="productIconWrapper">
-            <button type="button">
-              <FaRegHeart size={15} />
-            </button>
+          <div className="productInfoWrapper">
+            <div className="productCategory">
+              <p>鑰匙圈</p>
+            </div>
+            <h4>商品名</h4>
+            <p>$100</p>
           </div>
+        </Link>
+        <div className="productIconWrapper">
+          <button type="button" onClick={() => console.log("favBtn clicked!")}>
+            <FaRegHeart size={20} />
+          </button>
         </div>
-      </Link>
+      </div>
     </>
   );
 }

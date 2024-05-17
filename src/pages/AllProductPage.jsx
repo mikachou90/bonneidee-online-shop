05ec6c27b45ebf0203d23605d266b9cb1a0d1690
mikachou <chou1005.tw@gmail.com>
2024-05-17 15:51:51 +0800

    add hover on menu

--- src/pages/AllProductPage.jsx
+++ src/pages/AllProductPage.jsx
@@ -1,23 +1,35 @@
 import "../styles/allProductPage.scss";
+import { useState } from "react";
 import ProductCard from "../components/ProductCard";
 export default function AllProductPage() {
+  const [productTitle, setProductTitle] = useState("全部商品");
   return (
     <>
       <div id="allProductPage">
         <section className="leftSeciton">
           <h1>商品分類</h1>
           <div className="sideMenu">
-            <button type="button">全部</button>
-            <button type="button">鑰匙圈</button>
-            <button type="button">包包</button>
-            <button type="button">奶嘴夾</button>
-            <button type="button">裝飾</button>
+            <button type="button" onClick={() => setProductTitle("全部商品")}>
+              全部
+            </button>
+            <button type="button" onClick={() => setProductTitle("鑰匙圈")}>
+              鑰匙圈
+            </button>
+            <button type="button" onClick={() => setProductTitle("包包")}>
+              包包
+            </button>
+            <button type="button" onClick={() => setProductTitle("奶嘴夾")}>
+              奶嘴夾
+            </button>
+            <button type="button" onClick={() => setProductTitle("室內裝飾")}>
+              室內裝飾
+            </button>
           </div>
         </section>
 
         <section className="rightSection">
           <div className="filterCategory">
-            <h1>全部商品</h1>
+            <h1>{productTitle}</h1>
           </div>
           <div className="allProducts">
             <ProductCard />

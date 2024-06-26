8a2d695b66af22040a400f35150c9196c0ce70d6
mikachou <chou1005.tw@gmail.com>
2024-06-26 12:12:52 +0800

    adjust footer style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,29 +6,23 @@ import { IoMailOutline } from "react-icons/io5";
 export default function Footer() {
   return (
     <footer>
-      <div className="logoWrapper">
-        <img src="/src/assets/logo.png" alt="logo"></img>
-        <p>好點子手作</p>
+      <div className="links">
+        <p>Contact us!</p>
+        <ul>
+          <a href="https://www.instagram.com/craft_bonne_idee/">
+            <FaInstagram className="icon" />
+          </a>
+          <a href="/">
+            <FaShoppingBasket className="icon" />
+          </a>
+          <a href="#">
+            <IoMailOutline className="icon" />
+          </a>
+        </ul>
       </div>
 
-      <div className="linkWrapper">
-        <p>Find us!</p>
-        <div className="link">
-          <ul>
-            <a href="#">
-              <FaInstagram className="icon" />
-            </a>
-            <a href="#">
-              <FaShoppingBasket className="icon" />
-            </a>
-            <a href="#">
-              <IoMailOutline className="icon" />
-            </a>
-          </ul>
-        </div>
-        <div className="noti">
-          <p>手作商品請耐心等候</p>
-        </div>
+      <div className="authContent">
+        <p>ⓒ 2024 Bonne idee by Mika Chou</p>
       </div>
     </footer>
   );

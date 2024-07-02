e8fe50da81c06e30aa72b6bf58b50b32e22cc630
mikachou <chou1005.tw@gmail.com>
2024-07-02 11:15:42 +0800

    add animation in error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,16 @@
+import "../styles/errorPage.scss";
+import Lottie from "lottie-react";
+import errorAnimation from "../assets/errorAnimation.json";
+import { Link } from "react-router-dom";
+
 export default function ErrorPage() {
   return (
-    <div>
-      <h1>Error happended!</h1>
+    <div id="errorPage">
+      <Lottie className="errorAnimation" animationData={errorAnimation} />
+      <h1>Opps... Error Occured!</h1>
+      <button className="errorBtn">
+        <Link to="/">Try Again</Link>
+      </button>
     </div>
   );
 }
